60e0ed5c206d462e420232e65dc8dd2a8c21b87b
LEE JUNE <mars102aq@gmail.com>
2025-05-12 00:17:38 +0900

    Initial commit

---
+++ src/main/java/com/flyby/ramble/RambleApplication.java
@@ -0,0 +1,13 @@
+package com.flyby.ramble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RambleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RambleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flyby/ramble/RambleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyby.ramble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
