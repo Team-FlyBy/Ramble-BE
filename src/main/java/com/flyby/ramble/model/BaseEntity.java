d678abc4d7318e87fd1e13ed6f85c18c0780cf68
LEE JUNE <mars102aq@gmail.com>
2025-05-13 16:58:01 +0900

    feat: Auditing fields 공통 관리를 위한 BaseEntity 생성 #7

---
+++ src/main/java/com/flyby/ramble/model/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.flyby.ramble.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private String createdAt;
+
+    @LastModifiedDate
+    private String modifiedAt;
+}
