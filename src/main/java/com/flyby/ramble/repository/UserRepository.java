9d26dd243e60ea3e19f0d5312bc584489aa0bcad
LEE JUNE <mars102aq@gmail.com>
2025-05-19 22:11:38 +0900

    feat: UserRepository 사용자 조회 메서드 추가 #11

--- src/main/java/com/flyby/ramble/repository/UserRepository.java
+++ src/main/java/com/flyby/ramble/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.flyby.ramble.repository;
 
+import com.flyby.ramble.model.OAuthProvider;
 import com.flyby.ramble.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByProviderAndProviderId(OAuthProvider provider, String providerId);
+
 }
