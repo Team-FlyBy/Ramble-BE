c81a9c9bbf1583c174d3f3b8aea11d4671972c88
LEE JUNE <mars102aq@gmail.com>
2025-05-20 13:28:17 +0900

    feat: User 엔티티에 유니크 제약 조건 추가 #11

--- src/main/java/com/flyby/ramble/model/User.java
+++ src/main/java/com/flyby/ramble/model/User.java
@@ -8,11 +8,16 @@ import java.util.UUID;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uq_provider_provider_id",
+                columnNames = {"provider", "provider_id"}
+        ))
 public class User extends BaseEntity {
 
     // TODO: validation 필요 (email 등)
     // TODO: age, gender 추가 필요
+    // TODO: 차단, 탈퇴, 삭제 등 비활성화 정보 필요
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,10 +36,10 @@ public class User extends BaseEntity {
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private OAuthProvider provider;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private String providerId;
 
     @Enumerated(EnumType.STRING)
@@ -49,6 +54,10 @@ public class User extends BaseEntity {
 
     @Builder
     public User(String username, String email, OAuthProvider provider, String providerId, Role role) {
+        if (username.isEmpty() || email.isEmpty() || provider == null || providerId.isEmpty()) {
+            throw new IllegalArgumentException("필수 필드는 null이 될 수 없습니다");
+        }
+
         this.username = username;
         this.email = email;
         this.provider = provider;
