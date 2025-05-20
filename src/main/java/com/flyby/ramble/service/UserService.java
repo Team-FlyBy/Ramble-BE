21efc65bb4944d7bff8377fce0003d342ca5ee50
LEE JUNE <mars102aq@gmail.com>
2025-05-20 14:04:02 +0900

    refactor: RuntimeException -> IllegalArgumentException 변경 #11

--- src/main/java/com/flyby/ramble/service/UserService.java
+++ src/main/java/com/flyby/ramble/service/UserService.java
@@ -17,6 +17,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     // TODO: 동일한 유저의 중복 생성 가능성 판단 후 수정
+    // TODO: 커스텀 예외 처리 추후 추가
     public User registerOrLogin(String email, String username, OAuthProvider provider, String providerId) {
         try {
             return userRepository.findByProviderAndProviderId(provider, providerId)
@@ -29,7 +30,7 @@ public class UserService {
                             .build()));
         } catch (DataIntegrityViolationException e) {
             return userRepository.findByProviderAndProviderId(provider, providerId)
-                    .orElseThrow(() -> new RuntimeException("예상치 못한 오류가 발생했습니다", e));
+                    .orElseThrow(() -> new IllegalArgumentException("예상치 못한 오류가 발생했습니다", e));
         }
     }
 
