d80b12a5eb6d3d8b0929751c8bcac1c21d4a442f
LEE JUNE <mars102aq@gmail.com>
2025-05-20 13:30:01 +0900

    feat: OAuthProvider에 from 메서드 추가 #11
    - 지원하는 OAuth 제공자 확인 기능 구현

--- src/main/java/com/flyby/ramble/model/OAuthProvider.java
+++ src/main/java/com/flyby/ramble/model/OAuthProvider.java
@@ -5,5 +5,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum OAuthProvider {
     GOOGLE,
-    APPLE
+    APPLE;
+
+    public static OAuthProvider from(String provider) {
+        for (OAuthProvider oAuthProvider : OAuthProvider.values()) {
+            if (oAuthProvider.name().equalsIgnoreCase(provider)) {
+                return oAuthProvider;
+            }
+        }
+        throw new IllegalArgumentException("지원하지 않는 OAuth 제공자입니다: " + provider);
+    }
+
 }
