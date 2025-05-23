001897ef6180f37010f6d9e0ceebd25a3b161b54
LEE JUNE <mars102aq@gmail.com>
2025-05-23 22:02:17 +0900

    feat: JwtService 추가 및 토큰 생성 및 재발급 로직 구현 #9

---
+++ src/main/java/com/flyby/ramble/auth/service/JwtService.java
@@ -0,0 +1,91 @@
+package com.flyby.ramble.auth.service;
+
+import com.flyby.ramble.auth.model.RefreshToken;
+import com.flyby.ramble.auth.repository.RefreshTokenRepository;
+import com.flyby.ramble.auth.util.CookieUtil;
+import com.flyby.ramble.auth.util.JwtUtil;
+import com.flyby.ramble.common.exception.BaseException;
+import com.flyby.ramble.common.exception.ErrorCode;
+import com.flyby.ramble.common.model.DeviceType;
+import com.flyby.ramble.user.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final JwtUtil jwtUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    // TODO: DeviceType 구분 로직 필요, 현재는 WEB으로 고정
+    public void generateTokens(User user, HttpServletResponse response) {
+        String accToken  = jwtUtil.generateAccToken(user.getExternalId().toString(), user.getRole(), DeviceType.WEB, user.getProvider(), user.getProviderId());
+        String refToken  = jwtUtil.generateRefToken(user.getExternalId().toString(), user.getRole(), DeviceType.WEB, user.getProvider(), user.getProviderId());
+        Claims refClaims = parseRefreshToken(refToken);
+
+        refreshTokenRepository.save(createRefreshToken(user, refClaims));
+
+        populateResponse(accToken, refToken, response);
+    }
+
+    public void reissueTokens(HttpServletRequest request, HttpServletResponse response) {
+        String refToken  = CookieUtil.getCookie(request).orElseThrow(() -> new BaseException(ErrorCode.INVALID_REFRESH_TOKEN));
+        Claims refClaims = parseRefreshToken(refToken);
+        String jti       = refClaims.get("jti", String.class);
+        UUID   userId    = UUID.fromString(refClaims.getSubject());
+        DeviceType type  = DeviceType.valueOf(refClaims.get("deviceType", String.class));
+
+        Optional<RefreshToken> optionalToken = refreshTokenRepository.findByIdAndRevokedFalse(jti);
+
+        if (optionalToken.isEmpty()) {
+            refreshTokenRepository.revokeAllByUserIdAndDeviceType(userId, type);
+            throw new BaseException(ErrorCode.ACCESS_DENIED);
+        }
+
+        RefreshToken refreshToken = optionalToken.get();
+        refreshTokenRepository.save(refreshToken.revoke());
+        generateTokens(refreshToken.getUser(), response);
+    }
+
+    private Claims parseRefreshToken(String refreshToken) {
+        try {
+            return jwtUtil.parseClaims(refreshToken);
+        } catch (ExpiredJwtException e) {
+            throw new BaseException(ErrorCode.EXPIRED_REFRESH_TOKEN);
+        } catch (Exception e) {
+            throw new BaseException(ErrorCode.INVALID_REFRESH_TOKEN);
+        }
+    }
+
+    private RefreshToken createRefreshToken(User user, Claims claims) {
+        LocalDateTime exp = claims.getExpiration()
+                .toInstant()
+                .atZone(ZoneId.of("Asia/Seoul"))
+                .toLocalDateTime();
+
+        return RefreshToken.builder()
+                .id(claims.get("jti", String.class))
+                .user(user)
+                .deviceType(DeviceType.WEB)
+                .expiresAt(exp)
+                .build();
+    }
+
+    private void populateResponse(String access, String refresh, HttpServletResponse response) {
+        response.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + access);
+        response.addCookie(CookieUtil.createCookie("refresh", refresh));
+        response.setStatus(jakarta.servlet.http.HttpServletResponse.SC_OK);
+    }
+
+}
