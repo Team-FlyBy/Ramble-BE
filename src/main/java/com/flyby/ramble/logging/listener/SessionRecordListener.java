55e96e73dba691b1592f3d07481f468b651c4ffe
nativegold <zxc6941@gmail.com>
2025-06-06 18:16:53 +0900

    style: 단순 들여쓰기 제거

--- src/main/java/com/flyby/ramble/logging/listener/SessionRecordListener.java
+++ src/main/java/com/flyby/ramble/logging/listener/SessionRecordListener.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class SessionRecordListener {
-
     private final SessionRecordService sessionRecordService;
 
     @EventListener
