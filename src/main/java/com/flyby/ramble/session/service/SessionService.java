4ae658664257df34630d33e3f06e4b586240feb1
LEE JUNE <mars102aq@gmail.com>
2025-08-31 16:15:57 +0900

    feat: 세션 생성 메서드 추가 및 사용자 정보를 기반으로 참가자 추가 #47

--- src/main/java/com/flyby/ramble/session/service/SessionService.java
+++ src/main/java/com/flyby/ramble/session/service/SessionService.java
@@ -1,19 +1,41 @@
 package com.flyby.ramble.session.service;
 
+import com.flyby.ramble.matching.dto.MatchingProfileDTO;
 import com.flyby.ramble.session.model.Session;
 import com.flyby.ramble.session.repository.SessionRepository;
+import com.flyby.ramble.user.model.User;
+import com.flyby.ramble.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SessionService {
     private final SessionRepository sessionRepository;
+    private final UserService userService;
 
     public Long getSessionIdBySessionUuid(UUID sessionUuid) {
         Session session = sessionRepository.findByExternalId(sessionUuid).orElseThrow(() -> new IllegalArgumentException("데이터가 존재하지 않습니다."));
         return session.getId();
     }
+
+    public Session createSession(MatchingProfileDTO userA, MatchingProfileDTO userB) {
+        User userAEntity = userService.getUserProxyById(userA.getId());
+        User userBEntity = userService.getUserProxyById(userB.getId());
+
+        Session session = Session.builder()
+                .startedAt(LocalDateTime.now())
+                .build();
+
+        session.addParticipant(userAEntity, userA.getGender(), userA.getRegion(), userA.getLanguage());
+        session.addParticipant(userBEntity, userB.getGender(), userB.getRegion(), userB.getLanguage());
+
+        return sessionRepository.save(session);
+    }
+
 }
