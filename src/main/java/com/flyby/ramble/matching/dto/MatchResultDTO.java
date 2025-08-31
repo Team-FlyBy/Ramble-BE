2c32baf1bc4f26e797ccf3c6453c1e5127293c0d
LEE JUNE <mars102aq@gmail.com>
2025-08-31 16:16:57 +0900

    feat: MatchResultDTO (매칭 결과 데이터 전송 객체) 추가 #47

---
+++ src/main/java/com/flyby/ramble/matching/dto/MatchResultDTO.java
@@ -0,0 +1,10 @@
+package com.flyby.ramble.matching.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Data Transfer Object for Match Request")
+public record MatchResultDTO(
+        @Schema(description = "결과") String status,
+        @Schema(description = "상대") String partnerInfo
+) {
+}
