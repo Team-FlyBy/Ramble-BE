18d2ab99142ac3bd5d57fee1d126749f4b7862e8
LEE JUNE <mars102aq@gmail.com>
2025-08-31 16:08:19 +0900

    feat: RedisTemplate에 ObjectMapper 설정 추가 및 직렬화 방식 개선 #47

--- src/main/java/com/flyby/ramble/common/config/RedisConfig.java
+++ src/main/java/com/flyby/ramble/common/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.flyby.ramble.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,17 +16,26 @@ public class RedisConfig {
     private final RedisConnectionFactory connectionFactory;
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(ObjectMapper objectMapper) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
 
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
         // Key-Value
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setValueSerializer(serializer);
 
         // Hash Key-Value
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(serializer);
 
         return redisTemplate;
     }
