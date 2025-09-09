b709cf1cce10a0c8afe939b5190e4f1227b3b21d
LEE JUNE <mars102aq@gmail.com>
2025-09-09 19:28:43 +0900

    chore: MatchResultDTO에 TODO 주석 추가하여 향후 개선 사항 명시 #47

--- src/main/java/com/flyby/ramble/matching/dto/MatchResultDTO.java
+++ src/main/java/com/flyby/ramble/matching/dto/MatchResultDTO.java
@@ -2,6 +2,9 @@ package com.flyby.ramble.matching.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+// TODO: status enum으로 변경 고려
+// TODO: partnerInfo를 UserInfoDTO로 변경 고려
+
 @Schema(description = "Data Transfer Object for Match Result")
 public record MatchResultDTO(
         @Schema(description = "결과") String status,
