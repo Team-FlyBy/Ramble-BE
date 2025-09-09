cabd46073bc59ba8eff1a3de10106c1596133cc1
LEE JUNE <mars102aq@gmail.com>
2025-09-09 16:53:09 +0900

    refactor: MatchResultDTO의 설명을 매칭 결과에 맞게 수정

--- src/main/java/com/flyby/ramble/matching/dto/MatchResultDTO.java
+++ src/main/java/com/flyby/ramble/matching/dto/MatchResultDTO.java
@@ -2,7 +2,7 @@ package com.flyby.ramble.matching.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "Data Transfer Object for Match Request")
+@Schema(description = "Data Transfer Object for Match Result")
 public record MatchResultDTO(
         @Schema(description = "결과") String status,
         @Schema(description = "상대") String partnerInfo
