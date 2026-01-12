7e7c151e4b99c8bf3dbc3efa5e115a4fb458e31c
LEE JUNE <mars102aq@gmail.com>
2026-01-12 21:34:57 +0900

    refactor: MatchRequestDTO에서 지역 및 언어 타입을 enum으로 변경 #83

--- src/main/java/com/flyby/ramble/matching/dto/MatchRequestDTO.java
+++ src/main/java/com/flyby/ramble/matching/dto/MatchRequestDTO.java
@@ -1,12 +1,14 @@
 package com.flyby.ramble.matching.dto;
 
+import com.flyby.ramble.matching.model.Language;
+import com.flyby.ramble.matching.model.Region;
 import com.flyby.ramble.user.model.Gender;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Data Transfer Object for Match Request")
 public record MatchRequestDTO(
-        @Schema(description = "사용자가 선택한 지역") String region,
+        @Schema(description = "사용자가 선택한 지역") Region region,
         @Schema(description = "사용자가 선택한 성별") Gender gender,
-        @Schema(description = "사용자가 설정한 앱, 웹 언어") String language
+        @Schema(description = "사용자가 설정한 앱, 웹 언어") Language language
 ) {
 }
