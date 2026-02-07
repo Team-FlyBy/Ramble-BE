1defddf45ccd5b94e60a03d38242d9b02a470598
LEE JUNE <mars102aq@gmail.com>
2026-02-07 23:29:43 +0900

    refactor: MatchingController 및 SignalingController 리팩토링 #83

--- src/main/java/com/flyby/ramble/matching/controller/MatchingController.java
+++ src/main/java/com/flyby/ramble/matching/controller/MatchingController.java
@@ -1,73 +1,61 @@
 package com.flyby.ramble.matching.controller;
 
+import com.flyby.ramble.common.annotation.SwaggerApi;
+import com.flyby.ramble.common.dto.ResponseDTO;
 import com.flyby.ramble.common.service.GeoIpService;
+import com.flyby.ramble.common.util.ResponseUtil;
 import com.flyby.ramble.matching.dto.MatchRequestDTO;
+import com.flyby.ramble.matching.dto.MatchResultDTO;
+import com.flyby.ramble.matching.model.Region;
 import com.flyby.ramble.matching.service.MatchingService;
-import com.flyby.ramble.matching.dto.SignalMessageDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-import java.util.Optional;
-
-@Controller
+@Slf4j
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/match")
 public class MatchingController {
-
     private final GeoIpService geoIpService;
     private final MatchingService matchingService;
 
-    @MessageMapping("/match/request")
-    public void handleMatchRequest(@Payload MatchRequestDTO request,
-                                   SimpMessageHeaderAccessor headerAccessor,
-                                   Principal principal) {
-        String userId = getUserId(principal);
-        String region = geoIpService.getCountryCode(getUserIp(headerAccessor));
+    @SwaggerApi(
+            summary = "매칭 요청",
+            description = "매칭 요청 API"
+    )
+    @PostMapping("/request")
+    public ResponseEntity<ResponseDTO<MatchResultDTO>> handleMatchRequest(HttpServletRequest servletRequest,
+                                                                            @RequestBody MatchRequestDTO request,
+                                                                            @AuthenticationPrincipal UserDetails user) {
+        String userId = user.getUsername();
+        String region = geoIpService.getCountryCode(servletRequest.getRemoteAddr());
 
-        matchingService.findMatchOrAddToQueue(userId, region, request);
+        MatchResultDTO result = matchingService.requestMatch(userId, Region.from(region), request);
+
+        return ResponseUtil.success(result);
     }
 
-    @MessageMapping("/match/signaling")
-    public void handleSignalingMessage(@Payload SignalMessageDTO message, Principal principal) {
-        String userId = getUserId(principal);
+    @SwaggerApi(
+            summary = "매칭 취소",
+            description = "매칭 취소 요청 API",
+            responseCode = "204",
+            responseDescription = "No Content"
+    )
+    @PostMapping("/cancel")
+    public ResponseEntity<Void> handleCancelMatch(@AuthenticationPrincipal UserDetails user) {
+        String userId = user.getUsername();
 
-        matchingService.forwardSignalingMessage(userId, message);
-    }
+        matchingService.disconnectUser(userId);
 
-    @MessageMapping("/match/cancel")
-    public void handleCancelMatch(Principal principal) {
-        String userId = getUserId(principal);
-
-        matchingService.cleanupUser(userId);
-    }
-
-    private String getUserId(Principal principal) {
-        return Optional.ofNullable(principal)
-                .map(Principal::getName)
-                .orElseThrow(() -> new IllegalArgumentException("인증된 사용자 ID가 유효하지 않습니다."));
-    }
-
-    private String getUserIp(SimpMessageHeaderAccessor headerAccessor) {
-        if (headerAccessor == null) {
-            throw new IllegalArgumentException("헤더 액세서가 null 입니다.");
-        }
-
-        var sessionAttrs = headerAccessor.getSessionAttributes();
-
-        if (sessionAttrs == null) {
-            throw new IllegalArgumentException("세션 속성이 존재하지 않습니다.");
-        }
-
-        Object ip = sessionAttrs.get("ip");
-
-        if (ip == null) {
-            throw new IllegalArgumentException("세션에 IP 정보가 없습니다.");
-        }
-
-        return ip.toString();
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ src/main/java/com/flyby/ramble/matching/controller/SignalingController.java
@@ -0,0 +1,77 @@
+package com.flyby.ramble.matching.controller;
+
+import com.flyby.ramble.common.service.GeoIpService;
+import com.flyby.ramble.matching.dto.MatchRequestDTO;
+import com.flyby.ramble.matching.dto.SignalMessageDTO;
+import com.flyby.ramble.matching.model.Region;
+import com.flyby.ramble.matching.service.MatchingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class SignalingController {
+    private final GeoIpService geoIpService;
+    private final MatchingService matchingService;
+
+    @Deprecated(since = "2026-01-??", forRemoval = true)
+    @MessageMapping("/match/request")
+    public void handleMatchRequest(@Payload MatchRequestDTO request,
+                                   SimpMessageHeaderAccessor headerAccessor,
+                                   Principal principal) {
+        String userId = getUserId(principal);
+        String region = geoIpService.getCountryCode(getUserIp(headerAccessor));
+
+        matchingService.requestMatch(userId, Region.from(region), request);
+    }
+
+    @MessageMapping("/match/signaling")
+    public void handleSignalingMessage(@Payload SignalMessageDTO message, Principal principal) {
+        String userId = getUserId(principal);
+        message.setSenderId(userId);
+
+        matchingService.relaySignal(userId, message);
+    }
+
+    @Deprecated(since = "2026-01-??", forRemoval = true)
+    @MessageMapping("/match/cancel")
+    public void handleCancelMatch(Principal principal) {
+        String userId = getUserId(principal);
+
+        matchingService.disconnectUser(userId);
+    }
+
+    private String getUserId(Principal principal) {
+        return Optional.ofNullable(principal)
+                .map(Principal::getName)
+                .orElseThrow(() -> new IllegalArgumentException("인증된 사용자 ID가 유효하지 않습니다."));
+    }
+
+    private String getUserIp(SimpMessageHeaderAccessor headerAccessor) {
+        if (headerAccessor == null) {
+            throw new IllegalArgumentException("헤더 액세서가 null 입니다.");
+        }
+
+        var sessionAttrs = headerAccessor.getSessionAttributes();
+
+        if (sessionAttrs == null) {
+            throw new IllegalArgumentException("세션 속성이 존재하지 않습니다.");
+        }
+
+        Object ip = sessionAttrs.get("ip");
+
+        if (ip == null) {
+            throw new IllegalArgumentException("세션에 IP 정보가 없습니다.");
+        }
+
+        return ip.toString();
+    }
+
+
+}
